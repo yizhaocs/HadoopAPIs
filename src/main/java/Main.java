6a60741e720bb73a29a2d7b7c42e380108deab98
Yi Zhao <yizhao.cs@gmail.com>
2015-04-02 19:00:57 -0700

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		WriterAndReader mWriterAndReader = new WriterAndReader();
+		mWriterAndReader.writeToSequenceFile("a");
+		mWriterAndReader.readSequenceFile();
+	}
+}
